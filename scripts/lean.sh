99a63d089209a20408236a29e9e67c1dfb96d6dc
骷髅头 <lasstp5011@gmail.com>
2023-04-25 11:41:39 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -10,12 +10,13 @@
 # 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
 
 # mt7921
-rm -rf package/kernel/rtl8821cu
 rm -rf package/kernel/mac80211
+rm -rf package/kernel/mt76
 rm -rf package/network/services/hostapd
-svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
-svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
-
+svn export https://github.com/DHDAXCW/openwrt-beta/trunk/package/kernel/mac80211 package/kernel/mac80211
+chmod a-x package/kernel/mac80211/Makefile
+svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/kernel/mt76 package/kernel/mt76
+svn export https://github.com/DHDAXCW/openwrt-beta/trunk/package/network/services/hostapd package/network/services/hostapd
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
 rm -rf feeds/packages/lang/golang
